1a54d2447dbc99335d7bb3f56dea1d425bb3efd9
Nikoloz Nadashvili <nikoloznadashvili13@gmail.com>
2022-07-30 08:03:56 +0600

    commit_1

---
+++ index.js
@@ -0,0 +1,36 @@
+import { NFTStorage } from 'nft.storage'
+import { getFilesFromPath } from 'files-from-path'
+import * as pathModule from 'path';
+
+import { exit } from 'process';
+
+const token = '';
+
+async function main() {
+
+    if( token == '' ) {
+        console.log("please set your NFT storage API token");
+        return ;
+    }
+    const path = "./assets";
+    const files = await getFilesFromPath(path);
+    
+    for(var i = 0; i < files.length; i++ )
+      files[i].name = pathModule.basename(files[i].name);
+
+    const storage = new NFTStorage({ token })
+
+    console.log(`storing ${files.length} file(s) from ${path}`)
+    
+    const cid = await storage.storeDirectory(files, {
+        pathPrefix: path, // see the note about pathPrefix below
+        hidden: true
+    })
+
+    console.log({ cid })
+
+    const status = await storage.status(cid)
+    console.log(status)
+}
+
+main()
